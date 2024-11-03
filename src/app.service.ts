1f0ed8ae63fb055911e7bd9ca3d1d820a0e3f674
AchourOussama <oachour08@gmail.com>
2024-11-03 11:38:04 +0100

    Add: K8s manifests

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello to the weather app !';
+    return 'Hello to the weather app ! version 2 ';
   }
 }
