f544bd3335d57d2c5b420d299e9714909b0acf12
AchourOussama <oachour08@gmail.com>
2024-12-07 22:09:54 +0100

    Add : CORS permission

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  
   const app = await NestFactory.create(AppModule);
+  // Enable CORS for all origins
+  app.enableCors({
+    origin: 'http://localhost:4200',  // Allow requests from Angular frontend
+    methods: 'GET, POST, PUT, DELETE',  // Allow common HTTP methods
+    allowedHeaders: 'Content-Type, Authorization',  // Allow specific headers
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
