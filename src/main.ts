3e6909ed777aa18a526edef6f7d4daea90f9c39b
AchourOussama <oachour08@gmail.com>
2024-12-10 02:09:41 +0100

    Update: Add CORS and update Dockerfile

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Enable CORS for all origins
   app.enableCors({
-    origin: 'http://localhost:4200',  // Allow requests from Angular frontend
-    methods: 'GET, POST, PUT, DELETE',  // Allow common HTTP methods
-    allowedHeaders: 'Content-Type, Authorization',  // Allow specific headers
+    origin: ['http://localhost:4200','https://weather-app-frontend.azurewebsites.net'],  
+    methods: 'GET, POST, PUT, DELETE',  
+    allowedHeaders: 'Content-Type, Authorization',  
   });
   await app.listen(process.env.PORT ?? 3000);
 }
