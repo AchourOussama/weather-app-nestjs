fc3f84b80966aef696ec001ed06bac69ae79c49b
AchourOussama <oachour08@gmail.com>
2024-12-16 02:00:56 +0100

    Test: update the version to test the Jenkins pipeline

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello to the weather app ! version 2 ';
+    return 'Hello to the weather app ! version 3 ';
   }
 }
